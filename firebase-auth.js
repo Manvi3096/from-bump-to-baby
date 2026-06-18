7fee43fc629c93e7aa18674a53ef5772f7996912
Manvi3096 <manviacharya73@gmail.com>
2026-06-19 00:53:47 +0530

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,1 +1,31 @@
-console.log("Firebase auth loaded");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJOKnb1hJLPH1jCc9yJG5NGqRipI2QLvQ",
+  authDomain: "from-bump-to-baby.firebaseapp.com",
+  projectId: "from-bump-to-baby",
+  storageBucket: "from-bump-to-baby.firebasestorage.app",
+  messagingSenderId: "666764905793",
+  appId: "1:666764905793:web:dc05dfcd8e1065b05925c0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+window.firebaseAuth = {
+  auth,
+  provider,
+  signInWithPopup,
+  signOut
+};
+
+console.log("Firebase initialized");
